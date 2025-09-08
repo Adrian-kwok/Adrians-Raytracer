1fbef7499dacac13ea95a0595e772f71359ef387
adrian kwok <icantthinkgoodusername@gmail.com>
2025-09-07 21:11:29 -0400

    added all tuple functions except for cross product

--- tuple_test.cc
+++ tuple_test.cc
@@ -2,6 +2,69 @@
 #include "tuples.h"
 
 int main() {
-    tpoint tp = tpoint(1,2,3);
+    tuple tp = point(1, 2, 3);
     std::cout << tp.x << std::endl;
+    std::cout << tp.y << std::endl;
+    std::cout << tp.z << std::endl;
+    
+    if (tp.type == tuple_type::POINT) {
+        std::cout << "Code works!" << std::endl;
+    } else {
+        std::cout << "Uh oh..." << std::endl;
+    }
+
+    tuple tv = vector(1, 2, 3);
+    std::cout << tv.x << std::endl;
+    std::cout << tv.y << std::endl;
+    std::cout << tv.z << std::endl;
+
+    if (tv.type == tuple_type::VECTOR) {
+        std::cout << "Code works!" << std::endl;
+    } else {
+        std::cout << "Uh oh..." << std::endl;
+    }
+
+    tuple t = tp + tv;
+    std::cout << t.x << std::endl;
+    std::cout << t.y << std::endl;
+    std::cout << t.z << std::endl;
+    if (t.type == tuple_type::POINT) {
+        std::cout << "Code works!" << std::endl;
+    } else {
+        std::cout << "Uh oh..." << std::endl;
+    }
+
+    tuple foo = ZEROVEC + t + t;
+
+    std::cout << foo.x << std::endl;
+    std::cout << foo.y << std::endl;
+    std::cout << foo.z << std::endl;
+    if (foo.type == tuple_type::VECTOR) {
+        std::cout << "Code works!" << std::endl;
+    } else {
+        std::cout << "Uh oh..." << std::endl;
+    }
+
+    t = point(1, 2, 3);
+
+    foo = foo - t;
+    std::cout << foo.x << std::endl;    // 3, 6 , 9
+    std::cout << foo.y << std::endl;
+    std::cout << foo.z << std::endl;
+
+    foo = -foo;
+    std::cout << foo.x << std::endl;    // negative versions
+    std::cout << foo.y << std::endl;
+    std::cout << foo.z << std::endl;
+    foo = foo * (float(1) / float(3));
+    std::cout << foo.x << std::endl;    // something close to 1 2 3
+    std::cout << foo.y << std::endl;
+    std::cout << foo.z << std::endl;
+    foo = foo / 0.5;
+    std::cout << foo.x << std::endl;    // something close to 2 4 6
+    std::cout << foo.y << std::endl;
+    std::cout << foo.z << std::endl;
+    
+    std::cout << (foo == t) << std::endl; // false
+    std::cout << (-foo - t == t) << std::endl; // true
 }
--- tuples.h
+++ tuples.h
@@ -1,54 +1,38 @@
 #ifndef TUPLES
 #define TUPLES
 #include <cmath>
-#include "float_eq.cc"
 
-enum tuple_type {
-    POINT,
-    VECTOR,
-    COLOR
-};
+#include "float_eq.h"
+
+enum tuple_type { POINT, VECTOR, COLOR };
 
 struct tuple {
-    float x, y, z;
-    virtual tuple_type type() = 0;
-
-    tuple(float x, float y, float z);
+  float x, y, z;
+  tuple_type type;
 };
 
-struct tpoint : public tuple {
-    tuple_type type() { return tuple_type::POINT;}
-    tpoint(float x, float y, float z);
-};
+tuple vector(float x, float y, float z);
+tuple point(float x, float y, float z);
+tuple color(float x, float y, float z);
 
-struct tvector : public tuple {
-    tuple_type type() { return tuple_type::VECTOR;}
-    tvector(float x, float y, float z);
-};
-
-struct tcolor : public tuple {
-    tuple_type type() { return tuple_type::COLOR;}
-    tcolor(float x, float y, float z);
-};
-
-//const tvector ZEROVEC;
-
-tuple operator +(tuple t1, tuple t2);
-tuple operator -(tuple t1, tuple t2);
-tuple operator -(tuple t);
-tuple operator *(tuple t, float scalar);
+// Operators return a tuple which is the same type as the first parameter
+tuple operator+(tuple t1, tuple t2);
+tuple operator-(tuple t1, tuple t2);
+tuple operator-(tuple t);
+tuple operator*(tuple t, float scalar);
 // scalar cannot be 0, else 0 vector is returned
-tuple operator /(tuple t, float scalar);
-bool operator == (tuple t1, tuple t2);
+tuple operator/(tuple t, float scalar);
 
-float norm(tvector t);
-tvector normalize(tvector t);
+// doesn't check if tuples are of the same type
+bool operator==(tuple t1, tuple t2);
 
-float dot(tvector t1, tvector t2);
-tvector cross(tvector t1, tvector t2);
+float norm(tuple t);
+tuple normalize(tuple t);
 
+float dot(tuple t1, tuple t2);
+// always returns a vector
+tuple cross(tuple t1, tuple t2);
+
+const tuple ZEROVEC = vector(0, 0, 0);
 
 #endif
-
-
-
