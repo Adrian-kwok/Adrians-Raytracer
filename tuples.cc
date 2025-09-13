0be31e83f6921a756832bc2ce842b3ed093ba00f
adrian kwok <icantthinkgoodusername@gmail.com>
2025-09-13 17:30:09 -0400

    got rid of tuple types

--- tuples.cc
+++ tuples.cc
@@ -1,58 +1,39 @@
 #include "tuples.h"
 
-tuple point(float x, float y, float z) {
-  return tuple{x, y, z, tuple_type::POINT};
-}
-
-tuple vector(float x, float y, float z) {
-  return tuple{x, y, z, tuple_type::VECTOR};
-}
-
-tuple color(float x, float y, float z) {
-  return tuple{x, y, z, tuple_type::COLOR};
-}
-
 tuple operator+(tuple t1, tuple t2) {
-    return tuple{t1.x + t2.x, t1.y + t2.y, t1.z + t2.z, t1.type};
+  return tuple{t1.x + t2.x, t1.y + t2.y, t1.z + t2.z, t1.type + t2.type};
 }
 tuple operator-(tuple t1, tuple t2) {
-    return tuple{t1.x - t2.x, t1.y - t2.y, t1.z - t2.z, t1.type};
+  return tuple{t1.x - t2.x, t1.y - t2.y, t1.z - t2.z, t1.type - t2.type};
 }
-tuple operator-(tuple t) {
-    return tuple{-t.x, -t.y, -t.z, t.type};
-}
+tuple operator-(tuple t) { return tuple{-t.x, -t.y, -t.z, t.type}; }
 tuple operator*(tuple t, float scalar) {
-    return tuple{t.x * scalar, t.y * scalar, t.z * scalar, t.type};
+  return tuple{t.x * scalar, t.y * scalar, t.z * scalar, t.type};
 }
 // scalar cannot be 0, else 0 vector is returned
 tuple operator/(tuple t, float scalar) {
-    if (scalar == 0) {
-        return ZEROVEC;
-    } else {
-        return tuple{t.x / scalar, t.y / scalar, t.z / scalar, t.type};
-    }
+  if (scalar == 0) {
+    return ZEROVEC;
+  } else {
+    return tuple{t.x / scalar, t.y / scalar, t.z / scalar, t.type};
+  }
 }
 
 bool operator==(tuple t1, tuple t2) {
-    return (equal(t1.x, t2.x) && equal(t1.y, t2.y) && equal(t1.z, t2.z));
+  return (equal(t1.x, t2.x) && equal(t1.y, t2.y) && equal(t1.z, t2.z));
 }
 
-float norm(tuple t) {
-    return sqrt(t.x * t.x + t.y * t.y + t.z * t.z);
-}
+float norm(tuple t) { return sqrt(t.x * t.x + t.y * t.y + t.z * t.z); }
 
-tuple normalize(tuple t) {
-    return t / norm(t);
-}
+tuple normalize(tuple t) { return t / norm(t); }
 
 float dot(tuple t1, tuple t2) {
-    return t1.x * t2.x + t1.y * t2.y + t1.z * t2.z;
+  return t1.x * t2.x + t1.y * t2.y + t1.z * t2.z;
 }
 
 tuple cross(tuple t1, tuple t2) {
-    return vector(
-        (t1.y * t2.z) - (t1.z * t2.y),
-        (t1.z * t2.x) - (t1.x * t2.z),
-        (t1.x * t2.y) - (t1.y * t1.x)
-    );
+  return tuple{(t1.y * t2.z) - (t1.z * t2.y), 
+               (t1.z * t2.x) - (t1.x * t2.z),
+               (t1.x * t2.y) - (t1.y * t1.x), 
+               0};
 }
--- tuples.h
+++ tuples.h
@@ -4,16 +4,10 @@
 
 #include "float_eq.h"
 
-enum tuple_type { POINT, VECTOR, COLOR };
-
 struct tuple {
   float x, y, z;
-  tuple_type type;
+  int type;
 };
-
-tuple vector(float x, float y, float z);
-tuple point(float x, float y, float z);
-tuple color(float x, float y, float z);
 
 // Operators return a tuple which is the same type as the first parameter
 tuple operator+(tuple t1, tuple t2);
@@ -33,6 +27,6 @@ float dot(tuple t1, tuple t2);
 // always returns a vector
 tuple cross(tuple t1, tuple t2);
 
-const tuple ZEROVEC = vector(0, 0, 0);
+const tuple ZEROVEC {0, 0, 0, 0};
 
 #endif
