8503e2243bf851ca426ef922be820f21455dc3fa
adrian kwok <icantthinkgoodusername@gmail.com>
2025-09-13 19:00:56 -0400

    added colors

---
+++ color.h
@@ -0,0 +1,21 @@
+#ifndef COLOR
+#define COLOR
+
+#include "float_eq.h"
+#include <cmath>
+
+struct color {
+    float r, g, b;
+};
+
+color operator+(color c1, color c2);
+color operator-(color c1, color c2);
+color operator-(color c);
+color operator*(color c, float scalar);
+
+// mix colors
+color operator*(color c1, color c2);
+
+bool operator==(color c1, color c2);
+
+#endif
